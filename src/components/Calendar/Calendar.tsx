a933287b58b23a9c40fab7ec3d807cdc383691ce
muhammadusman3o0 <prof.fsbpcc0065@pgc.edu.pk>
2024-07-25 14:06:39 +0500

    refactor: Update Calendar component to improve interactivity and style

--- src/components/Calendar/Calendar.tsx
+++ src/components/Calendar/Calendar.tsx
@@ -16,20 +16,21 @@ const Calendar = () => {
   }, []);
 
   return (
-    <div className="bg-[url('/images/bgcal.png')] mt-20 py-10 bg-cover bg-no-repeat overflow-hidden relative">
-      <div className="min-h-full min-w-full bg-black/30   px-5 top-0 left-0 absolute" />
-      <div className="lg:grid-cols-2 grid gap-10 py-[50px] z-50 w-full items-center px-5">
-        <div className="z-50">
-          <p className="text-white font-semibold lg:text-[36px] text-[25px] mx-auto xl:text-[60px] max-w-[500px]">
-            Let's talk about your next big project
-          </p>
-        </div>
-        <div className="max-w-[540px]  rounded-[10px] mx-auto h-[700px] w-full z-50">
-          <div
-            className="calendly-inline-widget"
-            data-url={url}
-            style={{ minHeight: "650px", width: "100%", height: "700px" }}
-          ></div>
+    <div className="bg-[url('/images/bgcal.png')]  bg-cover bg-no-repeat overflow-hidden relative">
+      <div className="backdrop-brightness-50 pt-20 pb-5">
+        <div className="lg:grid-cols-2 grid gap-10 z-50 w-full items-center px-5">
+          <div className="z-50">
+            <p className="text-white font-semibold lg:text-[36px] text-[25px] mx-auto xl:text-[60px] max-w-[500px]">
+              Let's talk about your next big project
+            </p>
+          </div>
+          <div className="z-50 grid justify-center items-center">
+            <div
+              className="calendly-inline-widget"
+              data-url={url}
+              style={{ minHeight: "650px", width: "500px", height: "100%" }}
+            ></div>
+          </div>
         </div>
       </div>
     </div>
